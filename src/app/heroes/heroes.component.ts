d3122bb40bd825cf88176f80c06e4548bcfe3261
martinKindall <martin@rflex.cl>
2019-01-06 10:29:53 -0300

    Added list of heroes as mock. Listed them using ngFor

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -13,9 +14,17 @@ export class HeroesComponent implements OnInit {
 		name: 'Windstorm'
 	};
 
+  heroes = HEROES;
+
   constructor() { }
 
+  selectedHero: Hero;
+
   ngOnInit() {
+    // this.selectedHero = this.hero;
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
